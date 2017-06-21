6b7d07f8777804bf68e361a7b2a4923bdb39fbff
apostolos <apostolos_mon@hotmail.com>
2017-06-21 15:41:31 -0700

    Homework 1

--- Myapplication/app/src/main/java/com/example/apostolos/newsapp/MainActivity.java
+++ Myapplication/app/src/main/java/com/example/apostolos/newsapp/MainActivity.java
@@ -14,112 +14,76 @@ import android.widget.TextView;
 import java.io.IOException;
 import java.net.URL;
 
-
-
 public class MainActivity extends AppCompatActivity {
-
-    private EditText mSearchBoxEditText;
-
-    private TextView mUrlDisplayTextView;
-
-    private TextView mSearchResultsTextView;
-
-
-    private TextView mErrorMessageDisplay;
-
-
-    private ProgressBar mLoadingIndicator;
+    private ProgressBar progress;
+    private EditText search;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mSearchBoxEditText = (EditText) findViewById(R.id.et_search_box);
-
-        mUrlDisplayTextView = (TextView) findViewById(R.id.tv_url_display);
-        mSearchResultsTextView = (TextView) findViewById(R.id.tv_newsapp_search_results_json);
-
-
-        mErrorMessageDisplay = (TextView) findViewById(R.id.tv_error_message_display);
-
-
-        mLoadingIndicator = (ProgressBar) findViewById(R.id.pb_loading_indicator);
+        progress =(ProgressBar) findViewById(R.id.progressBar);
+        search =(EditText)findViewById(R.id.searchQuery);
+        textView=(TextView)findViewById(R.id.displayJSON);
     }
-
-
-    private void makenewsapiSearchQuery() {
-        String newsapiQuery = mSearchBoxEditText.getText().toString();
-        URL newsapiSearchUrl = NetworkUtils.buildUrl(newsapiQuery);
-        mUrlDisplayTextView.setText(newsapiSearchUrl.toString());
-        new newsapiQueryTask().execute(newsapiSearchUrl);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.main,menu);
+        return true;
     }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int itemNumber = item.getItemId();
 
-
-    private void showJsonDataView() {
-        // First, make sure the error is invisible
-        mErrorMessageDisplay.setVisibility(View.INVISIBLE);
-        // Then, make sure the JSON data is visible
-        mSearchResultsTextView.setVisibility(View.VISIBLE);
+        if(itemNumber==R.id.search){
+            String s=search.getText().toString();
+            NetworkTask task = new NetworkTask(s);
+            task.execute();
+        }
+        return true;
     }
+    class NetworkTask extends AsyncTask<URL,Void,String>{
+        String query;
 
-
-    private void showErrorMessage() {
-
-        mSearchResultsTextView.setVisibility(View.INVISIBLE);
-
-        mErrorMessageDisplay.setVisibility(View.VISIBLE);
-    }
-
-    public class newsapiQueryTask extends AsyncTask<URL, Void, String> {
-
-
+        NetworkTask(String s){
+            query =s;
+        }
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
-            mLoadingIndicator.setVisibility(View.VISIBLE);
+            progress.setVisibility(View.VISIBLE);
+
         }
 
         @Override
         protected String doInBackground(URL... params) {
-            URL searchUrl = params[0];
-            String newsapiSearchResults = null;
-            try {
-                newsapiSearchResults = NetworkUtils.getResponseFromHttpUrl(searchUrl);
-            } catch (IOException e) {
+            String result=null;
+            URL url = NetworkUtils.makeURL(query,"stars");
+
+            try{
+                result  = NetworkUtils.getResponseFromHttpUrl(url);
+            }catch(IOException e){
                 e.printStackTrace();
             }
-            return newsapiSearchResults;
+
+            return result;
         }
 
         @Override
-        protected void onPostExecute(String newsapiSearchResults) {
-            // COMPLETED (27) As soon as the loading is complete, hide the loading indicator
-            mLoadingIndicator.setVisibility(View.INVISIBLE);
-            if (newsapiSearchResults != null && !newsapiSearchResults.equals("")) {
-
-                showJsonDataView();
-                mSearchResultsTextView.setText(newsapiSearchResults);
-            } else {
-
-                showErrorMessage();
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            progress.setVisibility(View.GONE);
+            if(s == null){
+                textView.setText("Sorry, no text was received ");
             }
+            else
+            {
+                textView.setText(s);
+            }
+
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int itemThatWasClickedId = item.getItemId();
-        if (itemThatWasClickedId == R.id.action_search) {
-            makenewsapiSearchQuery();
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
--- Myapplication/app/src/main/java/com/example/apostolos/newsapp/NetworkUtils.java
+++ Myapplication/app/src/main/java/com/example/apostolos/newsapp/NetworkUtils.java
@@ -1,7 +1,7 @@
 package com.example.apostolos.newsapp;
 
-
 import android.net.Uri;
+import android.util.Log;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -11,45 +11,42 @@ import java.net.URL;
 import java.util.Scanner;
 
 
+
+
+
 public class NetworkUtils {
+    public static final String TAG ="NetworkUtils";
 
-    final static String NEWSAPI_BASE_URL =
-            "https://newsapi.org/v1/articles?source=the-next-web&sortBy=latest";
+    public static final String BASE_URL="https://newsapi.org/v1/articles?";
+    public static final String PARAM_QUERY="source";
+    public static final String PARAM_SORT="sortBy";
+    public static final String apiKey="apiKey";
 
-    final static String PARAM_KEY = "apiKey";
-    final static String KEY = "1e13a1ec462543d8a1b1100a0445c62f";
 
-    public static URL buildUrl(String newsapiQuery) {
-        Uri builtUri = Uri.parse(NEWSAPI_BASE_URL).buildUpon()
-                .appendQueryParameter(PARAM_KEY,KEY)
-                .build();
+    public static URL makeURL(String searchQuery, String sortBy){
+        Uri uri= Uri.parse(BASE_URL).buildUpon().appendQueryParameter(PARAM_QUERY,"the-next-web")
+                .appendQueryParameter(PARAM_SORT,"latest")
+                .appendQueryParameter(apiKey,"1e13a1ec462543d8a1b1100a0445c62f").build();
 
-        URL url = null;
-        try {
-            url = new URL(builtUri.toString());
+        URL url =null;
+        try{
+            String urlString= uri.toString();
+            Log.d(TAG,"URL" +urlString);
+            url = new URL(uri.toString());
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
-
         return url;
     }
+    public static String getResponseFromHttpUrl(URL url) throws IOException{
+        HttpURLConnection urlConnection =(HttpURLConnection) url.openConnection();
+        try{
+            InputStream in =urlConnection.getInputStream();
+            Scanner input = new Scanner(in);
 
-
-    public static String getResponseFromHttpUrl(URL url) throws IOException {
-        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-        try {
-            InputStream in = urlConnection.getInputStream();
-
-            Scanner scanner = new Scanner(in);
-            scanner.useDelimiter("\\A");
-
-            boolean hasInput = scanner.hasNext();
-            if (hasInput) {
-                return scanner.next();
-            } else {
-                return null;
-            }
-        } finally {
+            input.useDelimiter("\\A");
+            return(input.hasNext()) ? input.next():null;
+        }finally{
             urlConnection.disconnect();
         }
     }
